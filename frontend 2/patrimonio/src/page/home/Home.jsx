bd89b1349f56ab8f28f90f4bdbc0344b4b56b5d6
HugolinoBG <hugolino2609@gmail.com>
2024-02-21 17:06:14 -0300

    criacão fo fontend

--- frontend 2/patrimonio/src/page/home/Home.jsx
+++ frontend 2/patrimonio/src/page/home/Home.jsx
@@ -1,10 +1,37 @@
+import {useEffect, useState} from "react"
+import api from "../../services/api.jsx"
 import "./Home.css"
 
+
 function Home() {
+  const [patrimony, setPatrimony] = useState([])
+
+  useEffect(()=>{
+    api
+      .get("/patrimony")
+      .then()
+      .catch("")
+  })
   return (
     <>
       <div className="container">
-        <h1>Hello Wold!!</h1>
+        <div className="title">
+          <h1>Gerenciamento de Patrimônios</h1>
+        </div>
+        <div>
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Pedidos</th>
+                <th>Data</th>
+                <th>Total</th>
+              </tr>
+            </thead>
+            <tbody>
+
+            </tbody>
+          </table>
+        </div>
       </div>
     </>
   )
